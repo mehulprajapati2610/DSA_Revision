8d864e790b32a1617dc6223f93f58f7bbea311be
mehul26102005 <prajapatimehul2610@gmail.com>
2026-06-04 10:32:44 +0530

    formate the code

--- Array/SpiralMatrix.java
+++ Array/SpiralMatrix.java
@@ -1,10 +1,10 @@
 public class SpiralMatrix {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        spiralMatrix(arr);
+        spiralMatrix ( arr );
     }
 
-    public static void spiralMatrix(int[][] arr){
+    public static void spiralMatrix(int[][] arr) {
         int m = arr.length;
         int n = arr[0].length;
         int top = 0;
@@ -12,32 +12,32 @@ public class SpiralMatrix {
         int left = 0;
         int right = n - 1;
 
-        while(top <= bottom && left <= right){
+        while (top <= bottom && left <= right) {
 
             //right move
-            for(int i=left; i <= right; i++){
-                System.out.print (arr[top][i]+" ");
+            for (int i = left ; i <= right ; i++) {
+                System.out.print ( arr[top][i] + " " );
             }
             top++;
 
             //bottom move
-            for(int i=top; i <= bottom; i++){
-                System.out.print (arr[i][right]+" ");
+            for (int i = top ; i <= bottom ; i++) {
+                System.out.print ( arr[i][right] + " " );
             }
             right--;
 
             //left move
-            if(top <= bottom){
-                for(int i=right; i >= left; i--){
-                    System.out.print (arr[bottom][i]+" ");
+            if (top <= bottom) {
+                for (int i = right ; i >= left ; i--) {
+                    System.out.print ( arr[bottom][i] + " " );
                 }
                 bottom--;
             }
 
             //up move
-            if(left <= right){
-                for(int i=bottom; i >= top; i--){
-                    System.out.print (arr[i][left]+" ");
+            if (left <= right) {
+                for (int i = bottom ; i >= top ; i--) {
+                    System.out.print ( arr[i][left] + " " );
                 }
                 left++;
             }
