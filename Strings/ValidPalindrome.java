1cb85d0c8c020e9ecb1983d2dd09984096528a47
mehul26102005 <prajapatimehul2610@gmail.com>
2026-06-05 10:01:45 +0530

    DAY-4 Q2 Solved

---
+++ Strings/ValidPalindrome.java
@@ -0,0 +1,26 @@
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println ( "Is palindrome? : " + isPalindrome ( s ) );
+    }
+
+    public static boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length () - 1;
+
+        while (start < end) {
+            if (!Character.isLetterOrDigit ( s.charAt ( start ) )) {
+                start++;
+            } else if (!Character.isLetterOrDigit ( s.charAt ( end ) )) {
+                end--;
+            } else {
+                if (Character.toLowerCase ( s.charAt ( start ) ) != Character.toLowerCase ( s.charAt ( end ) )) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
